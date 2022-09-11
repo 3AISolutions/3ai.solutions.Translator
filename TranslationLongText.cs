e132b8e23c7aab5023a798325f82da78b1ac170d
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-09-11 13:05:38 +0300

    Update TranslationLongText.cs

--- TranslationLongText.cs
+++ TranslationLongText.cs
@@ -1,5 +1,9 @@
 ﻿namespace _3ai.solutions.Translator
 {
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TranslationRichText : Attribute { }
+
     [AttributeUsage(AttributeTargets.Property)]
     public class TranslationLongText : Attribute { }
 }
